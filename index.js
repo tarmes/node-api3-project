2316f471454b30e597fe3bbe2e38a5e34678d52d
tarmes <traarmes@gmail.com>
2020-12-04 09:43:20 -0500

    heroku edits

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
-const server = require('./api/server')
+const dotenv = require('dotenv')
+dotenv.config()
 
-server.listen(4000, () => {
+const server = require('./api/server')
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
    console.log('\n* Server Running on http://localhost:4000 *\n')
 })
