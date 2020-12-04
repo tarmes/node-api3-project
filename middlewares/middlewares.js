766a729bb24fada769ae017273d2088fb6edc1fc
tarmes <traarmes@gmail.com>
2020-12-03 23:03:06 -0500

    validatePost

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -28,7 +28,13 @@ function validateUser(req, res, next) {
 }
 
 function validatePost(req, res, next) {
-   // do your magic!
+   if (!req.body) {
+      res.status(400).json({ message: "missing post data"})
+   } else if (!req.text) {
+      res.status(400).json({ message: "missing required text field"})
+   } else {
+      next()
+   }   
 }
 
 module.exports = {
